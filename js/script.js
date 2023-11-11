a5c33b3b9a91bfd5a6a02efcad36ac728fe0b164
Stephen Wiafe <swiafe.dev@gmail.com>
2023-11-11 10:57:54 -0500

    Refactor code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
-const grid = document.querySelector('.grid');
-
-for(let i = 1; i <= 16; i++){
+function makeGridCells(){
+  const grid = document.querySelector('.grid');
+  for(let i = 1; i <= 16; i++){
     for(let j = 1; j <= 16; j++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
         grid.appendChild(cell);
     }
+  }
 }
 
-const cells = document.querySelectorAll('.cell');
+function addHoverEffect(){
+  const cells = document.querySelectorAll('.cell');
 
-cells.forEach(cell => {
+  cells.forEach(cell => {
     cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'red';
-
+      cell.style.backgroundColor = 'red';
     });
-});
+  });
+}
+
+function main(){
+  makeGridCells();
+  addHoverEffect();
+}
+
+main();
+
 
 
 
