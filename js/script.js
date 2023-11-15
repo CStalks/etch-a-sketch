b45533bd7fa79dc07097db1778396938cfbc1865
Stephen Wiafe <swiafe.dev@gmail.com>
2023-11-14 21:42:42 -0500

    Add button to change number of cells

--- js/script.js
+++ js/script.js
@@ -1,27 +1,55 @@
 function makeGridCells(){
   const grid = document.querySelector('.grid');
+
   for(let i = 1; i <= 16; i++){
     for(let j = 1; j <= 16; j++){
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        grid.appendChild(cell);
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      grid.appendChild(cell);  
     }
   }
 }
+
+/*function makeNewGridCells(gridSize){
+  const grid = document.querySelector('.grid');
+  grid.style["grid-template-columns"] = "repeat(gridSize, 1fr)";
+  grid.style["grid-template-rows"] = "repeat(gridSize, 1fr)";
+ 
+
+  for(let i = 1; i <= gridSize; i++){
+    for(let j = 1; j <= gridSize; j++){
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      grid.appendChild(cell);  
+    }
+  }
+}*/
 
 function addHoverEffect(){
   const cells = document.querySelectorAll('.cell');
 
   cells.forEach(cell => {
     cell.addEventListener('mouseover', () => {
-      cell.style.backgroundColor = 'red';
+      cell.style.backgroundColor = 'grey';
     });
   });
 }
 
+function changeGridSize(){
+  const button = document.querySelector('.button-size');
+  
+  button.addEventListener('click', () => {
+    const gridSize = prompt('Enter your prefered grid size(up to', 16);
+    makeNewGridCells(gridSize);
+
+  });
+}
+
+
 function main(){
   makeGridCells();
   addHoverEffect();
+  //changeGridSize();
 }
 
 main();
