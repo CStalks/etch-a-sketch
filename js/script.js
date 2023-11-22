f0e8843874638952a193b7be5a80449d5f7b9fdb
Stephen Wiafe <swiafe.dev@gmail.com>
2023-11-22 13:37:15 -0500

    Change grid sizes dynamically

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-function makeGridCells(){
-  const grid = document.querySelector('.grid');
+const grid = document.querySelector('.grid');
+const button = document.querySelector('.button-size');
 
+//creates default grid to display on the screen
+function makeDefaultCells(){
   for(let i = 1; i <= 16; i++){
     for(let j = 1; j <= 16; j++){
       const cell = document.createElement('div');
@@ -8,53 +10,54 @@ function makeGridCells(){
       grid.appendChild(cell);  
     }
   }
+
+  //add background color effect to the cells 
+  let cells = document.querySelectorAll('.cell');
+  
+  cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+      cell.style.backgroundColor = 'mediumpurple';
+    });
+  });
 }
 
-/*function makeNewGridCells(gridSize){
-  const grid = document.querySelector('.grid');
-  grid.style["grid-template-columns"] = "repeat(gridSize, 1fr)";
-  grid.style["grid-template-rows"] = "repeat(gridSize, 1fr)";
- 
+
+function makeNewCells(gridSize){
+  //remove old cells to replace new cells
+  while(grid.firstChild){
+    grid.firstChild.remove();
+  }
 
   for(let i = 1; i <= gridSize; i++){
     for(let j = 1; j <= gridSize; j++){
       const cell = document.createElement('div');
       cell.classList.add('cell');
-      grid.appendChild(cell);  
+      cell.style.width = (100/ gridSize) + "%";
+      grid.appendChild(cell);
     }
   }
-}*/
 
-function addHoverEffect(){
-  const cells = document.querySelectorAll('.cell');
-
+  //add background color effect to the cells 
+  //change these two in seperate function with the one in the other function
+  let cells = document.querySelectorAll('.cell');
+  
   cells.forEach(cell => {
     cell.addEventListener('mouseover', () => {
-      cell.style.backgroundColor = 'grey';
+      cell.style.backgroundColor = 'mediumpurple';
     });
   });
 }
 
-function changeGridSize(){
-  const button = document.querySelector('.button-size');
-  
-  button.addEventListener('click', () => {
-    const gridSize = prompt('Enter your prefered grid size(up to', 16);
-    makeNewGridCells(gridSize);
+function etchASketch(){
+  makeDefaultCells();
 
+  button.addEventListener('click', () => {
+    let gridSize = parseInt(prompt("Enter choice of grid", 16));
+    makeNewCells(gridSize);
   });
 }
 
-
-function main(){
-  makeGridCells();
-  addHoverEffect();
-  //changeGridSize();
-}
-
-main();
-
-
-
+etchASketch();
+  
 
 
